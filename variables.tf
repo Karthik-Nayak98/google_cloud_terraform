7cc31ba0e1b659df372b85db634dad6fa5e11c0c
Karthik Nayak <nayakkarthik466@gmail.com>
2025-08-20 15:43:30 +0530

    Format the tf code.

--- variables.tf
+++ variables.tf
@@ -1,9 +1,9 @@
 variable "project_name" {
-    default = "gcp-terraform-demo"
-    description = "Name of the GCP project"
+  default     = "gcp-terraform-demo"
+  description = "Name of the GCP project"
 }
 
 variable "region" {
-    default = "us-central1"
-    description = "Region for the project"
+  default     = "us-central1"
+  description = "Region for the project"
 }
