826c606f31885c066642a048c631acc36c89c022
Karthik Nayak <nayakkarthik466@gmail.com>
2025-08-20 15:27:13 +0530

    Add providers and variables.tf file.

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "project_name" {
+    default = "gcp-terraform-demo"
+    description = "Name of the GCP project"
+}
+
+variable "region" {
+    default = "us-central1"
+    description = "Region for the project"
+}
