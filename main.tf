4c2dcfb50b83f51c4fb4128ee90d0428009e4df3
Karthik Nayak <nayakkarthik466@gmail.com>
2025-08-20 23:34:23 +0530

    Add main.tf file

---
+++ main.tf
@@ -0,0 +1,16 @@
+module "gcp_cicd" {
+  source                = "./modules/gcp_cicd"
+  project_name          = var.project_name
+  region                = var.region
+  gitpat_secret         = var.gitpat_secret
+  github_repository     = var.github_repository
+  github_repository_url = var.github_repository_url
+  gcp_services          = var.gcp_services
+  app_installation_id   = var.app_installation_id
+}
+
+module "gcp_cicd" {
+  source       = "./modules/permissions"
+  project_name = var.project_name
+  region       = var.region
+}
