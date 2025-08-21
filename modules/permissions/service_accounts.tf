07dbdb054596bbebbecaa3c4a977ee12db56b5ff
Karthik Nayak <nayakkarthik466@gmail.com>
2025-08-21 12:07:16 +0530

    create cloudbuild service account

---
+++ modules/permissions/service_accounts.tf
@@ -0,0 +1,7 @@
+resource "google_service_account" "cloudbuild_service_account" {
+  project                      = var.project_name
+  account_id                   = "cloudbuild-sa"
+  display_name                 = "Service account used for cloudbuild repository activities"
+  create_ignore_already_exists = true
+  depends_on                   = [google_project_service.gcp_services]
+}
