07ed4ad7b85914729b6d81ac6fe134dd6cc7dbad
Karthik Nayak <nayakkarthik466@gmail.com>
2025-08-21 12:06:07 +0530

    Update outputs

--- modules/permissions/outputs.tf
+++ modules/permissions/outputs.tf
@@ -2,3 +2,9 @@ output "gcp_enabled_apis" {
   description = "The status of each enabled GCP API."
   value       = google_project_service.gcp_services
 }
+
+
+output "cloudbuild_sa" {
+  description = "Service account for the cloud build"
+  value       = google_service_account.cloudbuild_service_account.name
+}
