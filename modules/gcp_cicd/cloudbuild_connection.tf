a5fe2e2229b7354bff09f2166eba7de1b0fb95b0
Karthik Nayak <nayakkarthik466@gmail.com>
2025-08-21 16:23:30 +0530

    add dependency before creating secret manager

--- modules/gcp_cicd/cloudbuild_connection.tf
+++ modules/gcp_cicd/cloudbuild_connection.tf
@@ -5,6 +5,7 @@ resource "google_secret_manager_secret" "github_token_secret" {
   replication {
     auto {}
   }
+  depends_on = [var.gcp_enabled_apis]
 }
 
 resource "google_secret_manager_secret_version" "github_token_secret_version" {
