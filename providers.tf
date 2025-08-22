728a70590e6d20e62a916f45e3f625de66310190
Karthik Nayak <nayakkarthik466@gmail.com>
2025-08-22 12:09:22 +0530

    use google-beta hashicorp

--- providers.tf
+++ providers.tf
@@ -1,4 +1,19 @@
+terraform {
+  required_providers {
+    google = {
+      version = "~> 6.49.0"
+    }
+    google-beta = {
+      version = "~> 6.49.0"
+    }
+  }
+}
 provider "google" {
+  project = var.project_name
+  region  = var.region
+}
+
+provider "google-beta" {
   project = var.project_name
   region  = var.region
 }
