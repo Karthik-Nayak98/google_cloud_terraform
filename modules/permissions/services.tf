b4e98725949471b67096bbeaba4e19a5565efe22
Karthik Nayak <nayakkarthik466@gmail.com>
2025-08-22 12:56:42 +0530

    create cloudapi and serviceusage separately

--- modules/permissions/services.tf
+++ modules/permissions/services.tf
@@ -1,6 +1,18 @@
+
+resource "google_project_service" "serviceusage" {
+  project = var.project_name
+  service = "serviceusage.googleapis.com"
+}
+
+resource "google_project_service" "cloudapis" {
+  project    = var.project_name
+  service    = "cloudapis.googleapis.com"
+  depends_on = [google_project_service.serviceusage]
+}
 module "project-services" {
   source        = "terraform-google-modules/project-factory/google//modules/project_services"
   version       = "~> 18.0"
   project_id    = var.project_name
   activate_apis = var.gcp_services
+  depends_on    = [google_project_service.serviceusage, google_project_service.cloudapis]
 }
