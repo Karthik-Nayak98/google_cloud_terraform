791c602270d4c34b969aa75f3d5d75ceb47a57d6
Karthik Nayak <nayakkarthik466@gmail.com>
2025-08-22 12:15:43 +0530

    update depends-on in service accounts

--- modules/permissions/service_accounts.tf
+++ modules/permissions/service_accounts.tf
@@ -3,5 +3,5 @@ resource "google_service_account" "cloudbuild_service_account" {
   account_id                   = "cloudbuild-sa"
   display_name                 = "Service account used for cloudbuild repository activities"
   create_ignore_already_exists = true
-  depends_on                   = [google_project_service.gcp_services]
+  depends_on                   = [module.project-services]
 }
