742c5f78db512b6f30968ece29add2f096bf7aef
Karthik Nayak <nayakkarthik466@gmail.com>
2025-08-22 12:09:44 +0530

    use module project-services

--- modules/permissions/outputs.tf
+++ modules/permissions/outputs.tf
@@ -1,6 +1,6 @@
 output "gcp_enabled_apis" {
   description = "The status of each enabled GCP API."
-  value       = google_project_service.gcp_services
+  value       = module.project-services
 }
 
 
