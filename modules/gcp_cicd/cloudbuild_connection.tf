eb8b93da7f22894da365a0dba9b302ef23597b74
Karthik Nayak <nayakkarthik466@gmail.com>
2025-08-25 16:11:19 +0530

    define policy to access the secret and attach it to the secret.

--- modules/gcp_cicd/cloudbuild_connection.tf
+++ modules/gcp_cicd/cloudbuild_connection.tf
@@ -13,18 +13,19 @@ resource "google_secret_manager_secret_version" "github_token_secret_version" {
   secret_data = var.gitpat_secret
 }
 
-# data "google_iam_policy" "p4sa-secretAccessor" {
-#   binding {
-#     role = "roles/secretmanager.secretAccessor"
-#     // Here, 123456789 is the Google Cloud project number for the project that contains the connection.
-#     members = ["serviceAccount:service-123456789@gcp-sa-cloudbuild.iam.gserviceaccount.com"]
-#   }
-# }
+# Define the IAM policy to allow Cloud Build's service account to access the secret
+data "google_iam_policy" "p4sa-secretAccessor" {
+  binding {
+    role    = "roles/secretmanager.secretAccessor"
+    members = ["serviceAccount:service-${var.project_number}@gcp-sa-cloudbuild.iam.gserviceaccount.com"]
+  }
+}
 
-# resource "google_secret_manager_secret_iam_policy" "policy" {
-#   secret_id   = google_secret_manager_secret.github-token-secret.secret_id
-#   policy_data = data.google_iam_policy.p4sa-secretAccessor.policy_data
-# }
+# Attach the policy to the secret
+resource "google_secret_manager_secret_iam_policy" "policy" {
+  secret_id   = google_secret_manager_secret.github-token-secret.secret_id
+  policy_data = data.google_iam_policy.p4sa-secretAccessor.policy_data
+}
 
 resource "google_cloudbuildv2_connection" "github_repo_connection" {
   project  = var.project_name
