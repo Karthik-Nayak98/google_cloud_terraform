6f0c80b2661e2157b6acf3494f80432a8d1b87ad
Karthik Nayak <nayakkarthik466@gmail.com>
2025-08-28 23:41:42 +0530

    add developerconnect admin role to sa

--- modules/permissions/bindings.tf
+++ modules/permissions/bindings.tf
@@ -14,6 +14,10 @@ module "projects_iam_bindings" {
     "roles/secretmanager.secretAccessor" = [
       "serviceAccount:${google_service_account.cloudbuild_service_account.email}",
       "serviceAccount:service-${var.project_number}@gcp-sa-cloudbuild.iam.gserviceaccount.com",
+    ], 
+    "roles/developerconnect.admin" = [
+      "serviceAccount:${google_service_account.cloudbuild_service_account.email}",
+      "serviceAccount:service-${var.project_number}@gcp-sa-cloudbuild.iam.gserviceaccount.com",
     ]
   }
 
