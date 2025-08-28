45ea55f734a27ac7830c8d611756b353955e0f2f
Karthik Nayak <nayakkarthik466@gmail.com>
2025-08-28 23:34:18 +0530

    update the parent_connection for cloudbuild repo

--- modules/gcp_cicd/cloudbuild_trigger.tf
+++ modules/gcp_cicd/cloudbuild_trigger.tf
@@ -15,5 +15,6 @@ resource "google_cloudbuild_trigger" "ghe-trigger" {
 
   filename = "cloudbuild.yaml"
 
-  depends_on = [var.project_iam_bindings, google_cloudbuildv2_connection.github_repo_connection]
+  # depends_on = [var.project_iam_bindings, google_cloudbuildv2_connection.github_repo_connection]
+  depends_on = [var.project_iam_bindings, google_developer_connect_connection.github_repo_connection]
 }
