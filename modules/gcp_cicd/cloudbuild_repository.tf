9e3c09335b7fc5556de2a7dba907093e1607d139
Karthik Nayak <nayakkarthik466@gmail.com>
2025-08-31 15:37:12 +0530

    add depends_on

--- modules/gcp_cicd/cloudbuild_repository.tf
+++ modules/gcp_cicd/cloudbuild_repository.tf
@@ -3,6 +3,6 @@ resource "google_cloudbuildv2_repository" "github_devops_repo" {
   project           = var.project_name
   location          = var.region
   name              = var.github_repository
-  parent_connection = google_developer_connect_connection.github_repo_connection.id
+  parent_connection = google_cloudbuildv2_connection.github_repo_connection.id
   remote_uri        = var.github_repository_url
 }
