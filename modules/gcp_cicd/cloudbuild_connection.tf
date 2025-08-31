9b4a33f3b6813573cb20630fe53088eefe85f329
Karthik Nayak <nayakkarthik466@gmail.com>
2025-08-31 15:33:52 +0530

    add cloudbuild v2 connection

--- modules/gcp_cicd/cloudbuild_connection.tf
+++ modules/gcp_cicd/cloudbuild_connection.tf
@@ -10,30 +10,33 @@ resource "google_secret_manager_secret_version" "github-token-secret-version" {
   provider    = google-beta
   secret      = google_secret_manager_secret.github-token-secret.id
   secret_data = var.gitpat_secret
-  version     = "latest"
+  depends_on = [google_secret_manager_secret.github-token-secret]
 }
 
 data "google_iam_policy" "p4sa-secretAccessor" {
   binding {
-    role    = "roles/secretmanager.secretAccessor"
-    members = ["serviceAccount:service-${var.project_number}@gcp-sa-devconnect.iam.gserviceaccount.com"]
+    role = "roles/secretmanager.secretAccessor"
+    members = ["serviceAccount:service-${var.project_number}@gcp-sa-cloudbuild.iam.gserviceaccount.com"]
   }
 }
 
 resource "google_secret_manager_secret_iam_policy" "policy" {
-  secret_id   = google_secret_manager_secret.github-token-secret.secret_id
+  secret_id = google_secret_manager_secret.github-token-secret.secret_id
   policy_data = data.google_iam_policy.p4sa-secretAccessor.policy_data
 }
 
-resource "google_developer_connect_connection" "github_repo_connection" {
-  location      = var.region
-  connection_id = "github-devops-connection"
+resource "google_cloudbuildv2_connection" "github_repo_connection" {
+  provider = google-beta
+  project  = var.project_name
+  location = var.region
+  name     = "github-connection-gen2"
 
   github_config {
-    github_app          = "DEVELOPER_CONNECT"
-    app_installation_id = var.app_installation_id
+    app_installation_id = "${var.app_installation_id}"
     authorizer_credential {
       oauth_token_secret_version = google_secret_manager_secret_version.github-token-secret-version.id
     }
   }
+
+  depends_on = [var.project_iam_bindings, google_secret_manager_secret_version.githubpat-secret-secret-version]
 }
