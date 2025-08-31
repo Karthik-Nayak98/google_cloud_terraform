828f2dacb53306d6e74ffd90e068ca878e2d9220
Karthik Nayak <nayakkarthik466@gmail.com>
2025-08-31 15:33:39 +0530

    add cloudbuild service agent sa

--- modules/permissions/services.tf
+++ modules/permissions/services.tf
@@ -5,10 +5,10 @@ resource "google_project_service" "project_services" {
   disable_dependent_services = true
   disable_on_destroy         = true
 }
-# module "project-services" {
-#   source        = "terraform-google-modules/project-factory/google//modules/project_services"
-#   version       = "~> 18.0"
-#   project_id    = var.project_name
-#   activate_apis = var.gcp_services
-#   depends_on    = [google_project_service.serviceusage, google_project_service.cloudapis]
-# }
+
+resource "google_project_service_identity" "cloudbuild_service_agent_sa" {
+  provider   = google-beta
+  project    = var.project_name
+  service    = "cloudbuild.googleapis.com"
+  depends_on = [google_project_service.project_services]
+}
