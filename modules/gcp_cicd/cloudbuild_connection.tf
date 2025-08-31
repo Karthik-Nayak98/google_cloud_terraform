b4a3b4aad1f9cc0ecad10af9f73dad32150d0e32
Karthik Nayak <nayakkarthik466@gmail.com>
2025-08-31 17:05:51 +0530

    format code

--- modules/gcp_cicd/cloudbuild_connection.tf
+++ modules/gcp_cicd/cloudbuild_connection.tf
@@ -10,18 +10,18 @@ resource "google_secret_manager_secret_version" "github-token-secret-version" {
   provider    = google-beta
   secret      = google_secret_manager_secret.github-token-secret.id
   secret_data = var.gitpat_secret
-  depends_on = [google_secret_manager_secret.github-token-secret]
+  depends_on  = [google_secret_manager_secret.github-token-secret]
 }
 
 data "google_iam_policy" "p4sa-secretAccessor" {
   binding {
-    role = "roles/secretmanager.secretAccessor"
+    role    = "roles/secretmanager.secretAccessor"
     members = ["serviceAccount:service-${var.project_number}@gcp-sa-cloudbuild.iam.gserviceaccount.com"]
   }
 }
 
 resource "google_secret_manager_secret_iam_policy" "policy" {
-  secret_id = google_secret_manager_secret.github-token-secret.secret_id
+  secret_id   = google_secret_manager_secret.github-token-secret.secret_id
   policy_data = data.google_iam_policy.p4sa-secretAccessor.policy_data
 }
 
@@ -32,7 +32,7 @@ resource "google_cloudbuildv2_connection" "github_repo_connection" {
   name     = "github-connection-gen2"
 
   github_config {
-    app_installation_id = "${var.app_installation_id}"
+    app_installation_id = var.app_installation_id
     authorizer_credential {
       oauth_token_secret_version = google_secret_manager_secret_version.github-token-secret-version.id
     }
