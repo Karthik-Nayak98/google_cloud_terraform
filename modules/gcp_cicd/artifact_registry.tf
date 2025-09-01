a28786f72032ced1fbbb4b389ff31bb1be7a0b96
Karthik Nayak <nayakkarthik466@gmail.com>
2025-09-01 19:13:28 +0530

    add project to the artifact_registry module

--- modules/gcp_cicd/artifact_registry.tf
+++ modules/gcp_cicd/artifact_registry.tf
@@ -1,4 +1,5 @@
 resource "google_artifact_registry_repository" "frontend_backend_artifact" {
+  project       = var.project_name
   location      = var.region
   repository_id = "frontend_backend_repo"
   description   = "Registry for storing the frontend and backend images"
