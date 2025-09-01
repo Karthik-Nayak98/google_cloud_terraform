4872707be14aa4faa17f9a727889a4e2e49e055c
Karthik Nayak <nayakkarthik466@gmail.com>
2025-09-01 22:17:04 +0530

    update the artifactory repository name

--- modules/gcp_cicd/artifact_registry.tf
+++ modules/gcp_cicd/artifact_registry.tf
@@ -1,7 +1,7 @@
 resource "google_artifact_registry_repository" "frontend_backend_artifact" {
   project       = var.project_name
   location      = var.region
-  repository_id = "frontend_backend_repo"
+  repository_id = "frontend-backend-repo"
   description   = "Registry for storing the frontend and backend images"
   format        = "DOCKER"
 }
