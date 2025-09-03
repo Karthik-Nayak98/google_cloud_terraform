25795574545c6999fb6a247c13654d8dbfdffc4d
Karthik Nayak <nayakkarthik466@gmail.com>
2025-09-04 01:13:44 +0530

    change the sa role

--- modules/permissions/bindings.tf
+++ modules/permissions/bindings.tf
@@ -15,13 +15,11 @@ module "projects_iam_bindings" {
       "serviceAccount:${google_service_account.cloudbuild_service_account.email}",
       "serviceAccount:${var.terraform_service_account}",
       "serviceAccount:service-${var.project_number}@gcp-sa-cloudbuild.iam.gserviceaccount.com",
-      "serviceAccount:${var.project_number}@cloudbuild.iam.gserviceaccount.com"
     ],
     "roles/cloudbuild.builds.builder" = [
       "serviceAccount:${google_service_account.cloudbuild_service_account.email}",
       "serviceAccount:${var.terraform_service_account}",
       "serviceAccount:service-${var.project_number}@gcp-sa-cloudbuild.iam.gserviceaccount.com",
-      "serviceAccount:${var.project_number}@cloudbuild.iam.gserviceaccount.com"
     ],
     "roles/cloudbuild.serviceAgent" = [
       "serviceAccount:${google_service_account.cloudbuild_service_account.email}",
@@ -66,12 +64,14 @@ module "projects_iam_bindings" {
     "roles/logging.logWriter" = [
       "serviceAccount:${google_service_account.cloudbuild_service_account.email}",
       "serviceAccount:service-${var.project_number}@gcp-sa-cloudbuild.iam.gserviceaccount.com",
-      "serviceAccount:${var.project_number}@cloudbuild.iam.gserviceaccount.com"
     ],
     "roles/serviceusage.serviceUsageConsumer" = [
       "serviceAccount:${google_service_account.cloudbuild_service_account.email}",
       "serviceAccount:service-${var.project_number}@gcp-sa-cloudbuild.iam.gserviceaccount.com",
-      "serviceAccount:${var.project_number}@cloudbuild.iam.gserviceaccount.com"
+    ],
+    "roles/container.developer" = [
+      "serviceAccount:${google_service_account.cloudbuild_service_account.email}",
+      "serviceAccount:service-${var.project_number}@gcp-sa-cloudbuild.iam.gserviceaccount.com",
     ]
   }
 
