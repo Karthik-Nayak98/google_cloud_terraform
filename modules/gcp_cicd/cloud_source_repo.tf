fe3d7bd2707797841ab114c33c88d0cc0b6cc9a4
Karthik Nayak <nayakkarthik466@gmail.com>
2025-09-07 20:33:20 +0530

    remove cloudsource repo

--- modules/gcp_cicd/cloud_source_repo.tf
+++
@@ -1,5 +0,0 @@
-# resource "google_sourcerepo_repository" "frontend_backend_repo" {
-#   name       = "frontend_backend_devops"
-#   project    = var.project_name
-#   depends_on = [var.gcp_enabled_apis]
-# }
