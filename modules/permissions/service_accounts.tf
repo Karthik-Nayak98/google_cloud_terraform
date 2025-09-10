c99ad4ce70e570533dfe976dd9f3e0992111ac9c
Karthik Nayak <nayakkarthik466@gmail.com>
2025-09-10 23:27:51 +0530

    create gke service account

--- modules/permissions/service_accounts.tf
+++ modules/permissions/service_accounts.tf
@@ -5,3 +5,12 @@ resource "google_service_account" "cloudbuild_service_account" {
   create_ignore_already_exists = true
   depends_on                   = [google_project_service.project_services]
 }
+
+
+resource "google_service_account" "gke_service_account" {
+  project                      = var.project_name
+  account_id                   = "gke-node-pool-sa"
+  display_name                 = "Service account for GKE node pool"
+  create_ignore_already_exists = true
+  depends_on                   = [google_project_service.project_services]
+}
