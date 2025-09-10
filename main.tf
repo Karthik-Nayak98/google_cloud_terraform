91a5d1b2d0baf3f51254eec83f5c8e45376a3431
Karthik Nayak <nayakkarthik466@gmail.com>
2025-09-10 23:26:28 +0530

    add containers module

--- main.tf
+++ main.tf
@@ -10,16 +10,27 @@ module "permissions" {
   terraform_service_account = var.terraform_service_account
 }
 module "gcp_cicd" {
-  source                = "./modules/gcp_cicd"
-  project_name          = var.project_name
-  region                = var.region
-  gitpat_secret         = var.gitpat_secret
-  github_owner          = var.github_owner
-  github_repository     = var.github_repository
-  app_installation_id   = var.app_installation_id
-  gcp_enabled_apis      = module.permissions.gcp_enabled_apis
-  cloudbuild_sa         = module.permissions.cloudbuild_sa
-  project_iam_bindings  = module.permissions.iam_binding
-  project_number        = local.project_number
+  source               = "./modules/gcp_cicd"
+  project_name         = var.project_name
+  region               = var.region
+  gitpat_secret        = var.gitpat_secret
+  github_owner         = var.github_owner
+  github_repository    = var.github_repository
+  app_installation_id  = var.app_installation_id
+  gcp_enabled_apis     = module.permissions.gcp_enabled_apis
+  cloudbuild_sa        = module.permissions.cloudbuild_sa
+  project_iam_bindings = module.permissions.iam_binding
+  project_number       = local.project_number
 }
 
+module "containers" {
+  source              = "./modules/containers"
+  project_name        = var.project_name
+  region              = var.region
+  gke_service_account = module.permissions.gke_service_account
+  subnet_cidr         = var.subnet_cidr
+  pods_cidr           = var.pods_cidr
+  node_pool           = var.node_pool
+  cluster_name        = var.cluster_name
+  gke_version         = var.gke_version
+}
